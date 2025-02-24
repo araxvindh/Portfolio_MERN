76a403f908c01b00ba541c05589704485448c47c
araxvindh <aravindhswamy22@gmail.com>
2025-02-24 08:20:07 +0530

    the skills files has been added

--- Portfolio/src/components/functionalComponents/Skills.jsx
+++ Portfolio/src/components/functionalComponents/Skills.jsx
@@ -1,24 +1,24 @@
-import "..//css/skills.css"
-
+import "../css/skills.css"
 const Skills = () => {
     return (
-        <div className="skill-dic">
-            <h2>Programming Language</h2>
-            <ul>
-                <li><h3>C</h3></li>
-                <li><h3>C++</h3></li>
-                <li><h3>Java</h3></li>
-                <li><h3>Python</h3></li>
-            </ul>
-            <h2>Data Struture</h2>
-            <ul>
-                <li><h3>C++</h3></li>
-            </ul>
-            <h2>Tools</h2>
-            <ul><h3>VsCode</h3></ul>
-            <ul><h3>Git/Github</h3></ul>
+        <div className="skill-div">
+
+            <table>
+            <tr>
+                <th>Programming Language</th>
+                <td>C C++ JAVA PYTHON</td>
+            </tr>
             
-            
+            <tr>
+                <th>Data Structure</th>
+                <td>C++</td>
+            </tr>
+            <tr>
+            <th>Tools</th>
+            <td>Vscode Git/GitHub</td>
+            </tr>
+            </table>
+           
         </div>
     )
 }
